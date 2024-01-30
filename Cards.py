b3db748f70cd7b3ee36d48ca272dc4e56b85edcb
BoFo <brunofazio466@gmail.com>
2024-01-30 19:22:04 -0300

    reconfig del proyecto-falta entorno venv

--- Cards.py
+++ Cards.py
@@ -3,33 +3,4 @@ class Card:
     
     values = ["anchoEspada", "anchoBasto", "7", "6", "5", "4", "3", "2", "1","anchoOro", "anchoCopa","12","11","10"]
 
-    def __init__(self, v, s):
-        """suit + value are ints"""
-        self.value = v
-        self.suit = s
-
-    def __lt__(self, c2):
-        if self.value < c2.value:
-            return True
-        if self.value == c2.value:
-            if self.suit < c2.suit:
-                return True
-            else:
-                return False
-        return False
-
-    def __gt__(self, c2):
-        if self.value > c2.value:
-            return True
-        if self.value == c2.value:
-            if self.suit > c2.suit:
-                return True
-            else:
-                return False
-        return False
-
-    def __repr__(self):
-        v = self.values[self.value] +\
-            " of " + \
-            self.suits[self.suit]
-        return v
+   
--- Player.py
+++ Player.py
@@ -1,5 +1,2 @@
 class Player:
-   def __init__(self, name):
-       self.wins = 0
-       self.card = None
-       self.name = name
+    
